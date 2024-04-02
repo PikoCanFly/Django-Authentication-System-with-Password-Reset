6a3fafdbbeed5bc9b95d707913e9da0e582c255c
MaiaPiko <maia.mlynczak@gmail.com>
2024-04-02 17:23:23 +0200

    custom basic authentication

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import index, registration_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", index, name="index"),
+    path("accounts/register/", registration_view, name="register")
+] 
